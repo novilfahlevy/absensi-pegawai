14e2f0b4de37d5ef5cdb107e219b2e8f9f4ceb9c
MirrorBottle <setiawanbayu66152@gmail.com>
2020-01-14 14:03:10 +0800

    Selesai menampilkan pegawai dan menambah pegawai

--- src/store/actions/pegawaiActions.js
+++ src/store/actions/pegawaiActions.js
@@ -1,6 +1,6 @@
 import API from './../api.js';
 
-export const tambahPegawaI = (pegawai) => {
+export const addPegawai = (pegawai) => {
     return (dispatch, getState) => {
         API.post(`user/store`, pegawai)
             .then(res => dispatch({ type: 'ADD_PEGAWAI_SUCCESS', res }))
--- src/store/reducers/pegawaiReducer.js
+++ src/store/reducers/pegawaiReducer.js
@@ -6,9 +6,19 @@ const state = {
 const pegawaiReducer = (initState = state, action) => {
     switch (action.type) {
         case 'ADD_PEGAWAI_SUCCESS':
+            Swal.fire(
+                'Berhasil!',
+                'Pegawai berhasil ditambahkan',
+                'success'
+            )
             console.log('tambah pegawai berhasil');
             return action.res;
         case 'ADD_PEGAWAI_ERROR':
+            Swal.fire({
+                icon: 'Gagal!',
+                title: 'Oops...',
+                text: 'Sesuatu terjadi! Coba sekali lagi...',
+            })
             console.log('tambah pegawai gagal');
             return action.err;
         default:
