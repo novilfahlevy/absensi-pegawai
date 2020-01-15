098774adbca3c8b505f340f9b304a49b19afdb5b
novilfahlevy <novilfreon@gmail.com>
2020-01-15 09:49:25 +0800

    menambah attribute disable pada tombol loading dan membuat dokumentasi tombol loading

--- src/components/ui/LoadingButton.jsx
+++ src/components/ui/LoadingButton.jsx
@@ -3,10 +3,23 @@ import React from 'react';
 import { Button } from 'reactstrap';
 import 'assets/css/components/ui/loading.css';
 
+// cara make
+/*
+
+<LoadingButton color="primary" condition={kondisi loading}>
+  Tulisan Tombol
+</LoadingButton>
+
+Attribute:
+color = warna tombol di bootstrap
+condition = kondisi untuk loading nya (true atau false), nentuin dia lagi loading atau gk.
+
+*/
+
 class LoadingButton extends React.Component {
   render() {
     return (
-      <Button color={this.props.color} className={`d-flex justify-content-center align-content-center ${this.props.className || ''}`} style={{ position: 'relative' }}>
+      <Button color={this.props.color} className={`d-flex justify-content-center align-content-center ${this.props.className || ''}`} style={{ position: 'relative' }} disabled={this.props.condition}>
         <span style={{ opacity: this.props.condition ? 1 : 0, position: 'absolute', left: '50%', transform: 'translate(-50%)' }}>
           <svg width="100" height="12" viewBox="0 0 415 89" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="loading">
