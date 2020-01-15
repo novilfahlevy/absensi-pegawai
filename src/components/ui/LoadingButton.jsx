75edca48b32c2644a18475ea926bf2a6864cc4ea
novilfahlevy <novilfreon@gmail.com>
2020-01-15 09:29:29 +0800

    mengubah struktur folder ui dan css nya

--- src/components/ui/LoadingButton.jsx
+++ src/components/ui/LoadingButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Button } from 'reactstrap';
-import 'assets/css/assets/ui/loading.css';
+import 'assets/css/components/ui/loading.css';
 
 class LoadingButton extends React.Component {
   render() {
