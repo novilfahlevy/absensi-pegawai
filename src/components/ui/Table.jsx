6d48728148cf1a1573339d1c754f2ac2d62e18fd
novilfahlevy <novilfreon@gmail.com>
2020-01-17 20:04:18 +0800

    membuat paginasi pada table

---
+++ src/components/ui/Table.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import overlayFactory from 'react-bootstrap-table2-overlay';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import { connect } from 'react-redux';
+import {
+  Button,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Badge
+} from 'reactstrap';
+import Loading from 'components/ui/Loading.jsx';
+
+const  remoteTable = (props) => {
+  const NoDataIndication = () => (
+    <div className="m-3" style={{ color: '#b2b2b2' }}>
+      <Loading />
+    </div>
+  );
+
+  return (
+    <BootstrapTable 
+      // remote
+      hover
+      bootstrap4 
+      keyField='id' 
+      data={ props.data || [] } 
+      columns={ props.columns }
+      className="table-flush"
+      headerClasses="thead-light"
+      wrapperClasses="table-responsive"
+      bordered={ false }
+      loading={ props.isLoaded }
+      noDataIndication={ () => <NoDataIndication /> }
+      pagination={ paginationFactory({ 
+        page: Math.ceil(props.data.length / 10), 
+        sizePerPage: 10, 
+        totalSize: props.data.length
+      }) }
+      onTableChange={ props => alert(props) }
+      overlay={ overlayFactory(
+        { 
+          spinner: true, 
+          styles: { 
+            overlay: (base) => (
+              {...base, background: 'rgba(255, 255, 255, 0.3)'}
+            ),
+            spinner: (base) => (
+              {
+                ...base,
+                width: '60px',
+                '& svg circle': {
+                  stroke: '#5E72E4'
+                }
+              }
+            )
+          } }) }
+    />
+  )
+}
+
+export default remoteTable;
--- src/views/pegawai/PegawaiIndex.jsx
+++ src/views/pegawai/PegawaiIndex.jsx
@@ -25,6 +25,7 @@ import API from '../../store/api.js';
 import PegawaiForm from './PegawaiForm'
 import Swal from 'sweetalert2';
 import { withRouter } from 'react-router-dom';
+import Table from 'components/ui/Table.jsx';
 class PegawaiIndex extends React.Component {
     state = {
         pegawai: [],
@@ -115,57 +116,8 @@ class PegawaiIndex extends React.Component {
                                             <Button color="primary">Cari</Button>
                                         </InputGroupAddon>
                                     </InputGroup>
-                                    <BootstrapTable keyField="id" data={pegawai} columns={columns}></BootstrapTable>
+                                    <Table data={pegawai} columns={columns}></Table>
                                 </CardBody>
-                                <CardFooter className="py-4">
-                                    <nav aria-label="...">
-                                        <Pagination
-                                            className="pagination justify-content-end mb-0"
-                                            listClassName="justify-content-end mb-0"
-                                        >
-                                            <PaginationItem className="disabled">
-                                                <PaginationLink
-                                                    href="#pablo"
-                                                    onClick={e => e.preventDefault()}
-                                                    tabIndex="-1"
-                                                >
-                                                    <i className="fas fa-angle-left" />
-                                                    <span className="sr-only">Previous</span>
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem className="active">
-                                                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
-                                                    1
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink
-                                                    href="#pablo"
-                                                    onClick={e => e.preventDefault()}
-                                                >
-                                                    2 <span className="sr-only">(current)</span>
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink
-                                                    href="#pablo"
-                                                    onClick={e => e.preventDefault()}
-                                                >
-                                                    3
-                        </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink
-                                                    href="#pablo"
-                                                    onClick={e => e.preventDefault()}
-                                                >
-                                                    <i className="fas fa-angle-right" />
-                                                    <span className="sr-only">Next</span>
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                        </Pagination>
-                                    </nav>
-                                </CardFooter>
                             </Card>
                         </div>
                     </Row>
