0aa22210166fec3c7f30f89012311da323af103c
novilfahlevy <novilfreon@gmail.com>
2020-01-19 10:00:52 +0800

    memberikan animasi pada dashboard header

--- src/components/Headers/DashboardHeader.jsx
+++ src/components/Headers/DashboardHeader.jsx
@@ -19,6 +19,7 @@ import React from "react";
 
 // reactstrap components
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+import FadeIn from 'react-fade-in';
 
 class DashboardHeader extends React.Component {
   render() {
@@ -28,73 +29,75 @@ class DashboardHeader extends React.Component {
           <Container fluid>
             <div className="header-body">
               {/* Card stats */}
-              <Row>
-                <Col lg="6" xl="4">
-                  <Card className="card-stats mb-4 mb-xl-0">
-                    <CardBody>
-                      <Row>
-                        <div className="col">
-                          <CardTitle tag="h5" className="text-uppercase text-muted mb-0">
-                            Total Pegawai
-                          </CardTitle>
-                          <span className="h1 font-weight-bold mb-0">
-                            21
-                          </span>
-                        </div>
-                        <Col className="col-auto">
-                          <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                            <i className="fas fa-user-tie" />
+              <FadeIn delay="100">
+                <Row>
+                  <Col lg="6" xl="4">
+                    <Card className="card-stats mb-4 mb-xl-0">
+                      <CardBody>
+                        <Row>
+                          <div className="col">
+                            <CardTitle tag="h5" className="text-uppercase text-muted mb-0">
+                              Total Pegawai
+                            </CardTitle>
+                            <span className="h1 font-weight-bold mb-0">
+                              21
+                            </span>
                           </div>
-                        </Col>
-                      </Row>
-                    </CardBody>
-                  </Card>
-                </Col>
+                          <Col className="col-auto">
+                            <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                              <i className="fas fa-user-tie" />
+                            </div>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+                  </Col>
 
-                <Col lg="6" xl="4">
-                  <Card className="card-stats mb-4 mb-xl-0">
-                    <CardBody>
-                      <Row>
-                        <div className="col">
-                          <CardTitle tag="h5" className="text-uppercase text-muted mb-0">
-                            Pegawai Lembur Hari Ini
-                          </CardTitle>
-                          <span className="h1 font-weight-bold mb-0">2</span>
-                        </div>
-                        <Col className="col-auto">
-                          <div className="icon icon-shape bg-purple text-white rounded-circle shadow">
-                            <i className="fas fa-moon" />
+                  <Col lg="6" xl="4">
+                    <Card className="card-stats mb-4 mb-xl-0">
+                      <CardBody>
+                        <Row>
+                          <div className="col">
+                            <CardTitle tag="h5" className="text-uppercase text-muted mb-0">
+                              Pegawai Lembur Hari Ini
+                            </CardTitle>
+                            <span className="h1 font-weight-bold mb-0">2</span>
                           </div>
-                        </Col>
-                      </Row>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col lg="6" xl="4">
-                  <Card className="card-stats mb-4 mb-xl-0">
-                    <CardBody>
-                      <Row>
-                        <div className="col">
-                          <CardTitle
-                            tag="h5"
-                            className="text-uppercase text-muted mb-0"
-                          >
-                            Total Absen Hari Ini
-                          </CardTitle>
-                          <span className="h1 font-weight-bold mb-0">
-                            19
-                          </span>
-                        </div>
-                        <Col className="col-auto">
-                          <div className="icon icon-shape bg-info text-white rounded-circle shadow">
-                            <i className="far fa-list-alt" />
+                          <Col className="col-auto">
+                            <div className="icon icon-shape bg-purple text-white rounded-circle shadow">
+                              <i className="fas fa-moon" />
+                            </div>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                  <Col lg="6" xl="4">
+                    <Card className="card-stats mb-4 mb-xl-0">
+                      <CardBody>
+                        <Row>
+                          <div className="col">
+                            <CardTitle
+                              tag="h5"
+                              className="text-uppercase text-muted mb-0"
+                            >
+                              Total Absen Hari Ini
+                            </CardTitle>
+                            <span className="h1 font-weight-bold mb-0">
+                              19
+                            </span>
                           </div>
-                        </Col>
-                      </Row>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
+                          <Col className="col-auto">
+                            <div className="icon icon-shape bg-info text-white rounded-circle shadow">
+                              <i className="far fa-list-alt" />
+                            </div>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                </Row>                                        
+              </FadeIn>
             </div>
           </Container>
         </div>
