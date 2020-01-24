37d3d3761919a50a6e79b23c2ab748f1b6b48fe4
MirrorBottle <setiawanbayu66152@gmail.com>
2020-01-24 10:49:10 +0800

    Selesai dengan menampilkan data permintaan lembur dan riwayat lembur dari database

--- src/views/lembur/Lembur.jsx
+++ src/views/lembur/Lembur.jsx
@@ -24,97 +24,33 @@ import Swal from 'sweetalert2';
 import FadeIn from 'components/hoc/FadeIn.jsx';
 import PermintaanLembur from './PermintaanLembur.jsx'
 import BootstrapTable from 'react-bootstrap-table-next';
+import moment from 'moment';
+import 'moment/locale/id';
 import CardsContainer from 'components/ui/CardsContainer.jsx';
+import API from 'store/api.js';
+import Loading from 'components/ui/Loading.jsx';
 // import paginationFactory from 'react-bootstrap-table2-paginator';
 
 class Lembur extends React.Component {
     state = {
         modalIsOpen: false,
-        lembur: Array(6).fill(null).map((a, i) => ({
-            id: i + 1,
-            tanggal: '2019-10-02',
-            nama_pegawai: 'Muhammad Novil Fahlevy'.slice(0, 24) + "...",
-            waktu: '08:30:00 - 16:15:00',
-            total_waktu: '08:15:00',
-            opsi: (
-                <UncontrolledDropdown>
-                    <DropdownToggle size="sm">
-                        <i className="fas fa-ellipsis-v"></i>
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                        <DropdownItem onClick={() => this.deleteLembur(i + 1)} style={{ cursor: 'pointer' }}>
-                            <i className="fas fa-trash-alt text-danger"></i>
-                            Hapus
-            </DropdownItem>
-                        <DropdownItem onClick={() => this.props.history.push('detail-Absensi')} style={{ cursor: 'pointer' }}>
-                            <i className="fas fa-list-alt text-primary"></i>
-                            Lihat Detail Absensi
-            </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-            )
-        }))
-    };
+        lembur: null,
+        requestedLembur: []
+    }
     toggleModal = () => {
         this.setState({ modalIsOpen: !this.state.modalIsOpen })
     }
-
-    deleteLembur = id => {
-        Swal.fire({
-            title: 'Apa anda yakin?',
-            text: "Data yang sudah dihapus tidak bisa dipulihkan kembali!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Ya, hapus!',
-            cancelButtonText: 'Gak jadi!',
-            reverseButton: true
-        }).then((result) => {
-            if (result.value) {
-                this.setState({ lembur: this.state.lembur.filter(l => l.id !== id) }, () => {
-                    Swal.fire(
-                        'Dihapus!',
-                        'Data sudah dihapus.',
-                        'success'
-                    )
-                });
-            }
-        });
-    };
-
+    componentDidMount() {
+        API().get('lembur')
+            .then(res => {
+                this.setState({
+                    lembur: res.data.data.others,
+                    requestedLembur: res.data.data.waiting
+                })
+            })
+            .catch(err => console.log(err))
+    }
     render() {
-        const columns = [{
-            dataField: 'id',
-            text: '#',
-            headerStyle: () => ({
-                width: '20px',
-                textAlign: 'center'
-            }),
-        }, {
-            dataField: 'tanggal',
-            text: 'Tanggal',
-            headerAlign: 'center',
-            align: 'center'
-        }, {
-            dataField: 'nama_pegawai',
-            text: 'Nama Pegawai',
-            headerAlign: 'center',
-            align: 'left'
-        }, {
-            dataField: 'waktu',
-            text: 'Waktu Kerja',
-            headerAlign: 'center',
-            align: 'center'
-        }, {
-            dataField: 'opsi',
-            text: 'Opsi',
-            headerStyle: () => {
-                return { width: '70px', textAlign: 'center' };
-            },
-            align: 'center'
-        }];
-
         return (
             <>
                 <Container className="mt--7">
@@ -134,70 +70,49 @@ class Lembur extends React.Component {
                                     </Row>
                                 </CardHeader>
                                 <CardBody>
-                                    <CardsContainer data={[...Array(32).fill(null).map((a, i) => (
-                                        {
-                                            nama: 'Fadhil Dhanendra ke ' + (i+1),
-                                            tanggal: '28 Januari, 2019',
-                                            jam_masuk: '20:00',
-                                            jam_keluar: '22:00',
-                                            keterangan: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque aliquam laudantium facilis consequatur enim vel nisi amet! Dolor, facilis corporis?',
-                                            diSetujui: true
-                                        }
-                                    ))]} card={data => (
-                                        <Card body className="mb-3">
+                                    {this.state.lembur === null ? <div className="d-flex justify-content-center"><Loading /></div> : <CardsContainer data={this.state.lembur} card={data => (
+                                        <Card body className="mb-3 shadow">
                                             <Row>
                                                 <Col lg={12} sm={12} className="col-6 col-sm-12">
-                                                    <CardTitle className="mb-2">
-                                                        <Row>
-                                                            <Col lg={8}>
-                                                                <h2>{data.nama}</h2>
-                                                            </Col>
-                                                            <Col lg={4} className="text-right">
-                                                                <span className="font-weight-bold d-block">{data.tanggal}</span>
-                                                                <span className="font-weight-bold">{data.jam_masuk}</span> - <span className="font-weight-bold">{data.jam_keluar}</span>
-                                                            </Col>
-                                                        </Row>
-                                                    </CardTitle>
-                                                    <CardText>
-                                                        <span className="font-weight-bold d-block mt-lg-4">Keterangan : </span>
-                                                        <p className="m-0">{data.keterangan}</p>
-                                                    </CardText>
+                                                    <Row>
+                                                        <Col className="col-4">
+                                                            <img
+                                                                alt="..."
+                                                                height="200"
+                                                                style={{ borderRadius: "10px" }}
+                                                                src={`http://127.0.0.1:8000/storage/lembur/${data.foto || 'default.jpg'}`}
+                                                            />
+                                                        </Col>
+                                                        <Col className="col-8">
+                                                            <CardTitle className="mb-2">
+                                                                <Row>
+                                                                    <Col lg={8}>
+                                                                        <h2>{data.name}</h2>
+                                                                    </Col>
+                                                                    <Col lg={4} className="text-right">
+                                                                        <span className="font-weight-bold d-block">{moment(data.tanggal).locale('id').fromNow()}</span>
+                                                                        <span className="font-weight-bold">{data.lembur_awal}</span> - <span className="font-weight-bold">{data.lembur_akhir}</span>
+                                                                    </Col>
+                                                                </Row>
+                                                            </CardTitle>
+                                                            <CardText>
+                                                                <span className="font-weight-bold d-block mt-lg-4">Keterangan : </span>
+                                                                <p className="m-0">{data.keterangan}</p>
+                                                            </CardText>
+                                                        </Col>
+                                                    </Row>
                                                 </Col>
                                                 <Col lg={12} sm={12} className="col-6 col-sm-12 d-flex justify-content-sm-start justify-content-lg-end align-items-center">
-                                                    <h2><Badge color={data.diSetujui ? 'success' : 'danger'}>{data.diSetujui ? 'DITERIMA' : 'DITOLAK'}</Badge></h2>
+                                                    <h2><Badge color={data.status === 'diterima' ? 'success' : 'danger'}>{data.status === 'diterima' ? 'DITERIMA' : 'DITOLAK'}</Badge></h2>
                                                 </Col>
                                             </Row>
                                         </Card>
-                                    )} />
-                                    {/* <Card body>
-                                        <Row>
-                                            <Col lg={12} sm={12} className="col-6 col-sm-12">
-                                                <CardTitle className="mb-2">
-                                                    <Row>
-                                                        <Col lg={8}>
-                                                            <h2>Novil Fahlevy</h2>
-                                                        </Col>
-                                                        <Col lg={4} className="text-right">
-                                                            <span className="font-weight-bold d-block">29 Januari, 2019</span>
-                                                            <span className="font-weight-bold">18:00</span> - <span className="font-weight-bold">22:00</span>
-                                                        </Col>
-                                                    </Row>
-                                                </CardTitle>
-                                                <CardText>
-                                                    <span className="font-weight-bold d-block mt-lg-4">Keterangan : </span>
-                                                    <p className="m-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque aliquam laudantium facilis consequatur enim vel nisi amet! Dolor, facilis corporis?</p>
-                                                </CardText>
-                                            </Col>
-                                            <Col lg={12} sm={12} className="col-6 col-sm-12 d-flex justify-content-sm-start justify-content-lg-end align-items-center">
-                                                <h2><Badge color="success">Diterima</Badge></h2>
-                                            </Col>
-                                        </Row>
-                                    </Card> */}
+                                    )} />}
                                 </CardBody>
                             </Card>
                         </Col>
                     </Row>
-                    <PermintaanLembur modal={this.state.modalIsOpen} toggle={this.toggleModal} />
+                    <PermintaanLembur data={this.state.requestedLembur} modal={this.state.modalIsOpen} toggle={this.toggleModal} />
                 </Container>
 
             </>
--- src/views/lembur/PermintaanLembur.jsx
+++ src/views/lembur/PermintaanLembur.jsx
@@ -3,66 +3,92 @@ import React from 'react';
 import Header from 'components/Headers/Header.jsx';
 
 import {
-  Container,
-  Row,
-  Col,
-  Card,
-  CardBody,
-  CardHeader,
-  Button,
-  CardText,
-  CardTitle,
-  Modal,
-  ModalHeader,
-  ModalBody
+    Container,
+    Row,
+    Col,
+    Card,
+    CardBody,
+    CardHeader,
+    Button,
+    CardText,
+    CardTitle,
+    Modal,
+    ModalHeader,
+    ModalBody
 } from 'reactstrap';
-
+import moment from 'moment';
+import 'moment/locale/id';
 import { withRouter, Link } from 'react-router-dom';
 
 class PermintaanLembur extends React.Component {
-  render() {
-    let styles = {
-      card: {
-        border: "none",
-        boxShadow: "-6px -6px 20px rgba(255, 255, 255, 1), 6px 6px 20px rgba(0, 0, 0, .1)",
-      }
+    render() {
+        let styles = {
+            card: {
+                border: "none",
+                boxShadow: "-6px -6px 20px rgba(255, 255, 255, 1), 6px 6px 20px rgba(0, 0, 0, .1)",
+            }
+        }
+        return (
+            <>
+                <Modal size="lg" isOpen={this.props.modal} toggle={this.props.toggle}>
+                    <ModalHeader toggle={this.props.toggle}>
+                        <h2 className="m-0">Permintaan Lembur</h2>
+                    </ModalHeader>
+                    <ModalBody>
+                        {this.props.data.map(lembur => {
+                            return (
+                                <Card body style={styles.card} className="mb-2">
+                                    <Row>
+                                        <Col lg={12} sm={12} className="col-6 col-sm-12">
+                                            <Row>
+                                                <Col className="col-4">
+                                                    <img
+                                                        alt="..."
+                                                        height="200"
+                                                        style={{ borderRadius: "10px" }}
+                                                        src={`http://127.0.0.1:8000/storage/lembur/${lembur.foto || 'default.jpg'}`}
+                                                    />
+                                                </Col>
+                                                <Col className="col-8">
+                                                    <CardTitle className="mb-2">
+                                                        <Row>
+                                                            <Col lg={6}>
+                                                                <h2>{lembur.name}</h2>
+                                                            </Col>
+                                                            <Col lg={6} className="text-right">
+                                                                <span className="font-weight-bold d-block">{moment(lembur.tanggal).locale('id').fromNow()}</span>
+                                                                <span className="font-weight-bold">{lembur.lembur_awal}</span> - <span className="font-weight-bold">{lembur.lembur_akhir}</span>
+                                                            </Col>
+                                                        </Row>
+                                                    </CardTitle>
+                                                    <CardText>
+                                                        <span className="font-weight-bold d-block mt-lg-4">Keterangan : </span>
+                                                        <p className="m-0">{lembur.keterangan}</p>
+                                                    </CardText>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                        <Col lg={12} sm={12} className="col-6 col-sm-12 d-flex justify-content-sm-start justify-content-lg-end align-items-center">
+                                            <Link className="text-white" to={``} style={{ marginRight: "1rem" }}>
+                                                <Button className="w-70 h-70 bg-success text-white">
+                                                    <i className="fas fa-check text-white"></i>
+                                                </Button>
+                                            </Link>
+                                            <Link className="text-white" to={``}>
+                                                <Button className="w-70 h-70 bg-danger text-white">
+                                                    <i className="fas fa-times text-white"></i>
+                                                </Button>
+                                            </Link>
+                                        </Col>
+                                    </Row>
+                                </Card>
+                            )
+                        })}
+                    </ModalBody>
+                </Modal>
+            </>
+        );
     }
-    return (
-      <>
-        <Modal size="lg" isOpen={this.props.modal} toggle={this.props.toggle}>
-          <ModalHeader toggle={this.props.toggle}>
-            <h2 className="m-0">Permintaan Lembur</h2>
-          </ModalHeader>
-          <ModalBody>
-            <Card body style={styles.card}>
-              <Row>
-                <Col lg={12} sm={12} className="col-6 col-sm-12">
-                  <CardTitle className="m-0">Fadhil Dhanendra</CardTitle>
-                  <CardText>
-                    <span className="font-weight-bold">20:00</span> - <span className="font-weight-bold">22:00</span>
-                    <span className="font-weight-bold d-block mt-2">Keterangan : </span>
-                    <p className="m-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque aliquam laudantium facilis consequatur enim vel nisi amet! Dolor, facilis corporis?</p>
-                  </CardText>
-                </Col>
-                <Col lg={12} sm={12} className="col-6 col-sm-12 d-flex justify-content-sm-start justify-content-lg-end align-items-center">
-                  <Link className="text-white" to={``} style={{ marginRight: "1rem" }}>
-                    <Button className="w-70 h-70 bg-success text-white">
-                      <i className="fas fa-check text-white"></i>
-                    </Button>
-                  </Link>
-                  <Link className="text-white" to={``}>
-                    <Button className="w-70 h-70 bg-danger text-white">
-                      <i className="fas fa-times text-white"></i>
-                    </Button>
-                  </Link>
-                </Col>
-              </Row>
-            </Card>
-          </ModalBody>
-        </Modal>
-      </>
-    );
-  }
 }
 
 export default PermintaanLembur;
