8366d9f9835e5fa41940b2dd7a738a171e76f4fc
MirrorBottle <setiawanbayu66152@gmail.com>
2020-01-28 12:08:44 +0800

    build fix error

--- src/store/api.js
+++ src/store/api.js
@@ -3,7 +3,7 @@ import user from 'user.js';
 
 export default () => {
     const newAxios = axios.create({
-        baseURL: `${process.env.REACT_APP_BASE_URL}api/`,
+        baseURL: `${process.env.REACT_APP_BASE_URL}backend/api/`,
         headers: { 'Authorization': `Bearer ${user('token') || ''}` },
     });
     return newAxios;
